7d7fd37a8a26f180c8e355e9c5fbfabd774475d9
Ian Kimani <ianmwng@gmail.com>
2023-12-01 16:17:21 +0300

    complete assignment

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,15 @@
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()  # Call the parent class's hello method
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        super().raise_hand()  # Call the parent class's raise_hand method
+        return '\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n'  # Return the string with newline characters after each "Pick me!"
